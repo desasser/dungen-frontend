ac509a4f3debc847794a154bb734e33a9729a537
Angel <skelliebunnie@gmail.com>
2021-02-23 12:03:33 -0800

    added tailwindcss & created template pages

---
+++ src/components/Tile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Tile() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/pages/SavedMaps/SavedMaps.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SavedMaps() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
