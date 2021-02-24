4755791e6506de13d88188f10249dabbfe45ee4e
Daniel Sasser <desasser79@gmail.com>
2021-02-24 10:28:34 -0800

    tweaked file names and adjusted small details

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MapBuilder from './pages/MapBuilder';
 import NavBar from "./components/NavBar/index"
-import Login from "./pages/Login/Login";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,18 +10,15 @@ function App() {
       {/* <header className="App-header">
         <h1 className="text-2xl text-header">DUNGEN: JUNK WIZARDS</h1>
       </header> */}
+      <NavBar />
       <Router>
         <Switch>
           {/* <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} /> */}
+          <Route exact path="/" component={Login}/>
           <Route exact path="/dashboard" component={MapBuilder} />
         </Switch>
       </Router>
-      <NavBar />
-      <header className="App-header">
-        <h1 className="text-2xl text-header">DUNGEN: JUNK WIZARDS</h1>
-        <Login />
-      </header>
     </div>
   );
 }
