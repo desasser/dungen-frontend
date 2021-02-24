47af27d0845222e29a1120c2b14a8065e790c756
Daniel Sasser <desasser79@gmail.com>
2021-02-24 15:08:00 -0800

    pulling from dev

--- src/components/SliderDrawer/index.js
+++ src/components/SliderDrawer/index.js
@@ -5,25 +5,36 @@ import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
 import ReorderIcon from '@material-ui/icons/Reorder';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { Link } from 'react-router-dom';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles, useTheme } from '@material-ui/core/styles';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   sideNav: {
-    top: 20,
+    top: 80,
     zIndex: 3,
-    left: 20,
+    right: 20,
     position: 'fixed',
   },
   paper: {
-    width: '300px',
+    width: '30%',
+    // backgroundColor: 'pink',
   },
   link: {
     color: 'black',
     textDecoration: 'none',
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
   }
-});
+}));
 
-const ButtonStyle = withStyles({
+const StyledButton = withStyles({
   root: {
     top: 20,
     zIndex: 3,
@@ -33,41 +44,55 @@ const ButtonStyle = withStyles({
   }
 })(IconButton)
 
+const TileDrawer = withStyles({
+  root: {
+    "& .MuiDrawer-paper": {
+    backgroundColor: '#bada55',
+    width: '30%'
+  }
+}})(Drawer)
+
 export default function SliderDrawer() {
   const classes = useStyles();
   const [state, setState] = React.useState({
     isDrawerOpened: false
   })
 
-  const toggleDrawerStatus = () => {
+  const handleDrawerOpen = () => {
     setState({
       isDrawerOpened: true,
     })
   }
 
-  const closeDrawer = () => {
+  const handleDrawerClose = () => {
     setState({
       isDrawerOpened: false,
     })
   }
 
   const { isDrawerOpened } = state;
+  const theme = useTheme();
   return (
     <div>
-      <div>
+      <div className={classes.sideNav}>
       {/* className={classes.sideNav} */}
-        <ButtonStyle onClick={toggleDrawerStatus}>
+        <IconButton onClick={handleDrawerOpen}>
           {!isDrawerOpened ? <ReorderIcon /> : null}
-        </ButtonStyle>
+        </IconButton>
       </div>
       <Divider />
-      <Drawer
+      <TileDrawer
         anchor='right'
-        variant="temporary"
+        variant='persistent'
         open={isDrawerOpened}
-        onClose={closeDrawer}
-        classes={{ paper: classes.paper }}
+        onClose={handleDrawerClose}
+        // classes={{ paper: classes.paper }}
       >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
         <Link to='/about' classes={classes.link}>
           <List>
             <ListItem button key='About Us'>
@@ -86,7 +111,7 @@ export default function SliderDrawer() {
             </ListItem>
           </List>
         </Link>
-      </Drawer>
+      </TileDrawer>
     </div>
   );
 }
