334fea44abc0ce5c09b02ea17623eb61d45ad331
Angel <skelliebunnie@gmail.com>
2021-02-25 12:48:02 -0800

    tile drag drom outside almost done!

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -4,18 +4,75 @@ import './style.css'
 import Tile from '../Tile';
  
 export default function Grid() {
-    // layout is an array of objects, see the demo for more complete usage
-    // const layout = [
-    //   {i: 'a', x: 0, y: 0, w: 1, h: 2},
-    //   {i: 'b', x: 1, y: 0, w: 1, h: 2},
-    //   {i: 'c', x: 2, y: 0, w: 1, h: 2}
-    // ];
+  const [currentTile, setCurrentTile] = useState({
+    i: 'tile0',
+    x: 0,
+    y: 0,
+    orientation: 1
+  });
 
-    return (
-      <GridLayout className="layout" colWidth={100} rowHeight={100}  width={1200} compactType={null} preventCollision={true} margin={[0,0]} style={{backgroundColor: "gainsboro"}}>
-        <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 1}}><Tile color="tomato" /></div>
-        <div key="b" data-grid={{x: 1, y: 0, w: 1, h: 1}}><Tile color="dodgerblue" /></div>
-        <div key="c" data-grid={{x: 2, y: 0, w: 1, h: 1}}><Tile color="hotpink" /></div>
-      </GridLayout>
-    )
+  // layout is an array of objects, see the demo for more complete usage
+  const tileLayout = [
+    {i: '1', environment: 'swamp', x: 0, y: 0, w: 1, h: 1, bg: "#a63a50"},
+    {i: '2', environment: 'swamp', x: 1, y: 0, w: 1, h: 1, bg: "#5941a9"},
+    {i: '3', environment: 'swamp', x: 0, y: 1, w: 1, h: 1, bg: "#dcf763"},
+    {i: '4', environment: 'swamp', x: 1, y: 1, w: 1, h: 1, bg: "#435058"},
+  ];
+
+  const handleDraggableDragStart = (e) => {
+    e.dataTransfer.setData("text/plain", "");
+  }
+
+  const handleOnDrop = (layoutItem, e) => {
+    console.log(e);
+    let targetGrid = document.querySelectorAll(".mapGrid")[0];
+    setCurrentTile({
+      ...currentTile,
+      i: layoutItem.i,
+      x: layoutItem.x,
+      y: layoutItem.y
+    });
+
+    console.log(currentTile);
+    // let newTile = React.createElement("div", {key: })
+
+    // targetGrid.appendChild(newTile);
+  }
+
+  return (
+    <div style={{display: "flex", flexFlow: "row wrap"}}>
+      <div id="mapGrid" style={{flex: "1 0 70%"}}>
+        <GridLayout 
+          className="mapGrid" 
+          layout={tileLayout}
+          colWidth={100} 
+          rowHeight={100}
+          width={1200}
+          compactType={null} 
+          preventCollision={false}
+          margin={[0,0]} 
+          isDroppable={true}
+          onDrop={handleOnDrop}
+          style={{backgroundColor: "gainsboro", minHeight: "400px", minWidth: "400px"}}
+        >
+          {tileLayout.map(item => <div key={item.i} data-key={item.i} data-environment={item.environment} data-orientation={1} style={{backgroundColor: `${item.bg}`}} draggable={true} droppable="true"><span className="top">{item.i} (1)</span> <span className="right">(2)</span> <span className="bottom">(3)</span> <span className="left">(4)</span></div>)}
+        </GridLayout>
+      </div>
+      <div style={{flex: "1 0 20%"}}>
+        <div 
+          className="droppable-element"
+          key="tile0"
+          tile="1"
+          environment="swamp"
+          orientation="1"
+          draggable={true}
+          unselectable="on"
+          // Firefox hack
+          onDragStart={e => handleDraggableDragStart(e)}
+        >
+          DRAG ME
+        </div>
+      </div>
+    </div>
+  )
 }
