0a34de209fa2835d1e64642c28121c80ba86a12c
That Guy Cal <calmanning@gmail.com>
2021-02-25 13:31:09 -0800

    set URLPREFIX const in the API file for back-end communication and deploy prep.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,18 +1,21 @@
 const axios = require ("axios")
-
+//local url communication
+const URL_PREFIX = "http://localhost:3030"
+//When ready, the deployed site will use the following:
+//const URL_PREFIX = "http://$SITE_NAME$.herokuapp.com"
 
 const API = {
 
     signup: newUser => {
         console.log(newUser)
-        return axios.post("http://localhost:3030/api/newUser", newUser)
+        return axios.post(`${URL_PREFIX}/api/newUser`, newUser)
     },
     login: userData => {
         console.log(userData)
-        return axios.post("http://localhost:3030/login", userData)
+        return axios.post(`${URL_PREFIX}/login`, userData)
     },
     getAuthToken: token =>{
-        return axios.get("http://localhost:3030/auth", {
+        return axios.get(`${URL_PREFIX}/auth`, {
             headers: {
                 authorization: `Bearer: ${token}`
             }
