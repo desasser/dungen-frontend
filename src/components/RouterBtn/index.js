62d43911b159906a758cfc6fb99a1c9aac215f59
Douglas Johnson <trunks22a@gmail.com>
2021-02-26 11:03:21 -0800

    Fixed Router Btn

--- src/components/RouterBtn/index.js
+++ src/components/RouterBtn/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
 import {
-  BrowserRouter as Router,
   Link
 } from "react-router-dom";
 
 
 export default function RouterBtn(props) {
   return (
-    <Router>
+
       <div>
-        <Button color="primary" component={props.component} to={props.route} className={props.classes}>
+        <Button color="primary" href={props.to}>
           {props.name}
         </Button>
       </div>
-    </Router>
+
   )
 }
