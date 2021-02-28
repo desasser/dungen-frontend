cd56e8851168d9d8047477e66adea67a0b9ec19f
Daniel Sasser <desasser79@gmail.com>
2021-02-28 13:10:03 -0800

    background image placeholder added

--- src/pages/RenderedMap/index.js
+++ src/pages/RenderedMap/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, withStyles, useTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  largeMap: {
+    backgroundColor: '#372248',
+    border: '1px black solid',
+    width: '80%',
+    height: 1000,
+    borderRadius: '0.5em',
+    backgroundImage: `url("http://paratime.ca/images/fantasy/dungeon-055.jpg")`,
+    backgroundRepeat: 'no-repeat',
+    // backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover'
+  },
+})
 
 export default function RenderedMap() {
+  const classes = useStyles();
+
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Typography variant='h2'>
+        This where you can see a big map!
+        <Container className={classes.largeMap}>
+
+        </Container>
+      </Typography>
+    </Container>
   )
 }
