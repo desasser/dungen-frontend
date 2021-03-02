fd4c2312c3bec1ae466bd801de6e7ef30e85645c
Daniel Sasser <desasser79@gmail.com>
2021-03-02 11:56:03 -0800

    title update works

--- src/pages/MapBuilder/index.js
+++ src/pages/MapBuilder/index.js
@@ -45,13 +45,27 @@ const useStyles = makeStyles({
     width: '600px',
     // fullWidth: true,
     '& .MuiFilledInput-input': {
-      fontSize:'24px',
-      fontFamily: 'SpaceAndAstronomy'
+      fontSize: '24px',
+      fontFamily: 'SpaceAndAstronomy',
+      backgroundColor: 'white'
     }
   },
   title: {
     fontFamily: 'SpaceAndAstronomy',
-    fontSize: '40px'
+    fontSize: '40px',
+    marginTop: 20
+  },
+  titleBtn: {
+    // margin: 10,
+    color: 'white',
+    alignSelf: 'flex-end',
+    marginRight: 20
+  },
+  titleWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    width: '80%'
   }
 })
 
@@ -73,15 +87,15 @@ export default function MapBuilder() {
   let { id } = useParams();
 
   React.useEffect(() => {
-    if(id !== undefined) {
+    if (id !== undefined) {
       console.log(id);
       API.getSingleMap(id)
-      .then(results => {
-        console.log(results.data);
-        // const loadedLayout = results.data
-      })
-      .catch(err => console.error(err));
-      }
+        .then(results => {
+          console.log(results.data);
+          // const loadedLayout = results.data
+        })
+        .catch(err => console.error(err));
+    }
   }, []);
 
   const handleLock = () => {
@@ -105,59 +119,66 @@ export default function MapBuilder() {
   const saveMapToDB = (e) => {
     // console.log(e.target);
     const mapLayout = JSON.parse(localStorage.getItem('dungen_map')).layout;
-    
-    API.saveMap({UserId: 1, name: mapTitle, image_url: ""})
-    .then(savedMap => {
-      console.log(savedMap.data);
-      const newMapId = savedMap.data.id;
-      
-      for(var i = 0; i < mapLayout.length; i++) {
-        let tile = {
-          MapId: newMapId,
-          TileId: parseInt(mapLayout[i].tileId),
-          xCoord: mapLayout[i].x,
-          yCoord: mapLayout[i].y,
-          orientation: mapLayout[i].orientation
+
+    API.saveMap({ UserId: 1, name: mapTitle, image_url: "" })
+      .then(savedMap => {
+        console.log(savedMap.data);
+        const newMapId = savedMap.data.id;
+
+        for (var i = 0; i < mapLayout.length; i++) {
+          let tile = {
+            MapId: newMapId,
+            TileId: parseInt(mapLayout[i].tileId),
+            xCoord: mapLayout[i].x,
+            yCoord: mapLayout[i].y,
+            orientation: mapLayout[i].orientation
+          }
+
+          API.saveMapTile(tile)
+            .then(savedMapTile => {
+              console.log(savedMapTile.data);
+            })
+            .catch(err => console.error(err));
         }
 
-        API.saveMapTile(tile)
-        .then(savedMapTile => {
-          console.log(savedMapTile.data);
-        })
-        .catch(err => console.error(err));
-        }
-
-    })
-    .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
   }
 
   const viewMap = (e) => {
     console.log(e.target);
   }
 
-  const updateMapTitle = (e) => {
-    setMapTitle(e.target.value);
-  }
-
   const clearMap = (e) => {
     console.log("clear the grid")
+  }
+
+  const handleTitleSubmit = (event) => {
+    event.preventDefault();
+    setTitleState(false);
   }
 
   return (
     <Container>
       <Container>
-        {!titleState ? <Button onClick={() => setTitleState(true)}>Edit Title</Button>:
-        <TextField id="filled-basic" label="Map Title" variant="filled" value={mapTitle} onChange={(e) => updateMapTitle(e)} className={classes.titleInput} onSubmit={()=> setTitleState(false)}/>}
-        <Typography variant='h2' className={classes.title}>{mapTitle}</Typography>
+        <Container className={classes.titleWrapper}>
+          <Typography variant='h2' className={classes.title}>{mapTitle}</Typography>
+          {!titleState ? <Button onClick={() => setTitleState(true)} className={classes.titleBtn}>Edit Title</Button> :
+            <form onSubmit={handleTitleSubmit}>
+              <TextField id="filled-basic" label="Map Title" variant="filled" value={mapTitle} onChange={(e) => setMapTitle(e.target.value)} className={classes.titleInput} />
+            </form>}
+        </Container>
+
         {/* The "handleDraggableItem" prop here is to get the data for the AddThisTile const */}
         <SliderDrawer handleDraggableItem={handleDraggableItem} />
         {/* GRID BOX */}
         <Container className="grid-base" style={{ border: 'black 1px solid', height: '1000px', width: '1000px', marginLeft: '0px', marginTop: '25px', padding: '0px' }}>
           <Grid addThisTile={addThisTile} loadThisMap={loadThisMap} />
         </Container>
-        <IconBtn name='icon' classes={classes.iconBtn} onClick={handleLock}>
+        {/* TODO: This functionality is for future development */}
+        {/* <IconBtn name='icon' classes={classes.iconBtn} onClick={handleLock}>
           {lockState ? <LockOutlinedIcon /> : <LockOpenOutlinedIcon />}
-        </IconBtn>
+        </IconBtn> */}
         <ActionBtn name='CLEAR' classes={classes.actionBtn} action={clearMap} />
         <ActionBtn name='SAVE' classes={classes.actionBtn} action={saveMapToDB} />
         <RouterBtn name='VIEW' classes={classes.routerBtn} action={viewMap} />
