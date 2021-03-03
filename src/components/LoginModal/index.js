a54c4c6d8d5fb8588f2ff0ab5ecf17cfd8e3156d
Daniel Sasser <desasser79@gmail.com>
2021-03-03 09:25:25 -0800

    styled login button

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -8,74 +8,82 @@ import Form from "../../components/Form/index"
 import ActionBtn from "../../components/ActionBtn/index"
 import { useHistory } from 'react-router-dom'
 import { Snackbar } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
-    modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    paper: {
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  navLink: {
+    textDecoration: 'none',
+    fontFamily: 'SpaceAndAstronomy',
+    fontSize: '20px',
+    marginLeft: '20px',
+    color: '#707078'
+  }
 }));
 
 export default function LoginModal(props) {
-console.log(props)
+  console.log(props)
 
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const history = useHistory(props);
-    useEffect(() => {
-        if (props.isLoggedIn) {
-            handleClose()
-        }
-    }, [props.isLoggedIn])
-
-    const email = () => {
-        console.log("this might work")
+  const history = useHistory(props);
+  useEffect(() => {
+    if (props.isLoggedIn) {
+      handleClose()
     }
+  }, [props.isLoggedIn])
 
-    return (
-        <div>
-            <MenuItem type="button" onClick={handleOpen}>
-                Login
+  const email = () => {
+    console.log("this might work")
+  }
+
+  return (
+    <div>
+      <MenuItem type="button" onClick={handleOpen} className={classes.navLink} >
+        Login
       </MenuItem>
-            <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                className={classes.modal}
-                open={open}
-                onClose={handleClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                    timeout: 500,
-                }}
-            >
-                <Fade in={open}>
-                    <div className={classes.paper}>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
 
-                        <ActionBtn action={props.switch} name={props.formBtn} />
-                        <h1>{props.formMsg}</h1>
-                        <Form login={props.login.login} user={props.user} error={props.error} handleSubmit={props.handleSubmit} handleInputChange={props.handleInputChange} />
+            <ActionBtn action={props.switch} name={props.formBtn} />
+            <h1>{props.formMsg}</h1>
+            <Form login={props.login.login} user={props.user} error={props.error} handleSubmit={props.handleSubmit} handleInputChange={props.handleInputChange} />
 
-                    </div>
-                </Fade>
-            </Modal>
-        </div>
-    );
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
