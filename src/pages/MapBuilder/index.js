7bf037751b6929c3486cf7076f6a497eae249994
Daniel Sasser <desasser79@gmail.com>
2021-03-03 18:34:05 -0800

    mobile map builder

--- src/pages/MapBuilder/index.js
+++ src/pages/MapBuilder/index.js
@@ -15,6 +15,7 @@ import Button from '@material-ui/core/Button';
 import SaveBar from '../../components/SaveBar'
 import AuthBar from '../../components/AuthBar'
 import API from '../../utils/API';
+import snail from '../../images/DisapproverSnail.png';
 
 const useStyles = makeStyles({
   tileGrid: {
@@ -30,7 +31,7 @@ const useStyles = makeStyles({
     color: '#E4572E',
   },
   actionBtn: {
-    '&:hover' :{
+    '&:hover': {
       color: 'white',
       backgroundColor: '#eb4511'
     },
@@ -42,7 +43,7 @@ const useStyles = makeStyles({
     fontSize: '18px',
   },
   routerBtn: {
-    '&:hover' :{
+    '&:hover': {
       color: '#36434b',
       backgroundColor: 'white'
     },
@@ -84,6 +85,25 @@ const useStyles = makeStyles({
     display: 'flex',
     justifyContent: 'flex-end',
     width: '94.5%'
+  },
+  imageStyle: {
+    padding: 40,
+    borderRadius: '0.25em',
+    width: '80%',
+    height: 'auto'
+  },
+  mobileMapBuilder: {
+    fontFamily: 'SpaceAndAstronomy',
+    fontSize: '40px',
+    marginTop: 20
+  },
+  mobileMapBuilderSub: {
+    fontSize: '14px',
+    fontFamily: 'SpaceAndAstronomy'
+  },
+  subTextWrapper: {
+    width: '60%',
+    marginTop: 10
   }
 })
 
@@ -287,35 +307,60 @@ export default function MapBuilder(props) {
     setTitleState(false);
   }
 
-  return (
-    <Container>
-      <Container>
-        <Container className={classes.titleWrapper}>
-          <Typography variant='h2' className={classes.title}>{mapTitle}</Typography>
-          {!titleState ? <Button onClick={() => setTitleState(true)} className={classes.titleBtn}>Edit Title</Button> :
-            <form onSubmit={handleTitleSubmit}>
-              <TextField id="filled-basic" label="Map Title" variant="filled" value={mapTitle} onChange={(e) => setMapTitle(e.target.value)} className={classes.titleInput} />
-            </form>}
-        </Container>
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 
-        {/* The "handleDraggableItem" prop here is to get the data for the AddThisTile const */}
-        <SliderDrawer handleDraggableItem={handleDraggableItem} />
-        {/* GRID BOX */}
-        <Container className="grid-base" style={{ outline: '#8eb1c7 15px solid', height: '1000px', width: '1000px', marginTop: '25px', padding: '0px' }}>
-          <Grid addThisTile={addThisTile} loadThisMap={id} viewState={viewState}/>
-        </Container>
-        {/* TODO: This functionality is for future development */}
-        {/* <IconBtn name='icon' classes={classes.iconBtn} onClick={handleLock}>
+  console.log('check me out!', navigator.userAgent);
+  console.log('mobile?', isMobile);
+
+  return (
+    !isMobile ?
+      (<Container>
+        <Container>
+          <Container className={classes.titleWrapper}>
+            <Typography variant='h2' className={classes.title}>{mapTitle}</Typography>
+            {!titleState ? <Button onClick={() => setTitleState(true)} className={classes.titleBtn}>Edit Title</Button> :
+              <form onSubmit={handleTitleSubmit}>
+                <TextField id="filled-basic" label="Map Title" variant="filled" value={mapTitle} onChange={(e) => setMapTitle(e.target.value)} className={classes.titleInput} />
+              </form>}
+          </Container>
+
+          {/* The "handleDraggableItem" prop here is to get the data for the AddThisTile const */}
+          <SliderDrawer handleDraggableItem={handleDraggableItem} />
+          {/* GRID BOX */}
+          <Container className="grid-base" style={{ outline: '#8eb1c7 15px solid', height: '1000px', width: '1000px', marginTop: '25px', padding: '0px' }}>
+            <Grid addThisTile={addThisTile} loadThisMap={id} viewState={viewState} />
+          </Container>
+          {/* TODO: This functionality is for future development */}
+          {/* <IconBtn name='icon' classes={classes.iconBtn} onClick={handleLock}>
           {lockState ? <LockOutlinedIcon /> : <LockOpenOutlinedIcon />}
         </IconBtn> */}
-        <Container className={classes.btnWrapper}>
-          {/* <ActionBtn name='CLEAR' classes={classes.actionBtn} action={clearMap} /> */}
-          <ActionBtn name={!viewState ? 'VIEW' : 'BUILD'} classes={classes.routerBtn} action={viewMap} />
-          <ActionBtn name='SAVE' classes={classes.actionBtn} action={saveMapToDB} />
+          <Container className={classes.btnWrapper}>
+            {/* <ActionBtn name='CLEAR' classes={classes.actionBtn} action={clearMap} /> */}
+            <ActionBtn name={!viewState ? 'VIEW' : 'BUILD'} classes={classes.routerBtn} action={viewMap} />
+            <ActionBtn name='SAVE' classes={classes.actionBtn} action={saveMapToDB} />
+          </Container>
         </Container>
-      </Container>
-      <SaveBar saved={saved} toggleSavedState={toggleSavedState} />
-      <AuthBar auth={auth} toggleAuthState={toggleAuthState} />
-    </Container>
+        <SaveBar saved={saved} toggleSavedState={toggleSavedState} />
+        <AuthBar auth={auth} toggleAuthState={toggleAuthState} />
+      </Container>) :
+      (
+        <Container>
+          <Typography variant='h3' className={classes.mobileMapBuilder}>
+            Sorry, you can't make maps on mobile... yet...
+        </Typography>
+          <Container className={classes.subTextWrapper}>
+            <Typography variant='h6' className={classes.mobileMapBuilderSub}>
+              Besides, do you really want to build a map with your thumb?
+        </Typography>
+          </Container>
+          <img
+            src={snail}
+            className={classes.imageStyle}
+          />
+        </Container>
+      )
+
   )
 }
